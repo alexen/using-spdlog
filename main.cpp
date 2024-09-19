70409f1f6f8aabfbfd37f4e17dd172f0ff4f2964
Enakaev Aleksey <enakaev.aleksey@yandex.ru>
2024-09-19 22:32:15 +0300

    main: Реализовано одновременное логгирование в stderr и в лог с ротацией

--- main.cpp
+++ main.cpp
@@ -1,11 +1,45 @@
+#include <string>
+#include <memory>
 #include <iostream>
+
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+
+
+void initializeLogger( const std::string& appName )
+{
+     static constexpr auto rotatingLogSize = 10u * 1024u * 1024u;
+     static constexpr auto maxRotatingFiles = 10u;
+     static constexpr auto rotateOnOpen = true;
+
+     auto stderrSink = std::make_shared< spdlog::sinks::stderr_color_sink_mt >();
+     stderrSink->set_level( spdlog::level::trace );
+     stderrSink->set_pattern( "[multi_sink_example] [%^%l%$] %v" );
+
+     auto rotatingFileSink = std::make_shared< spdlog::sinks::rotating_file_sink_mt >(
+          appName + ".log"
+          , rotatingLogSize
+          , maxRotatingFiles
+          , rotateOnOpen
+          );
+     rotatingFileSink->set_level( spdlog::level::trace );
+
+     spdlog::set_default_logger(
+          std::make_shared< spdlog::logger >(
+               appName
+               , spdlog::sinks_init_list{ stderrSink, rotatingFileSink }
+               )
+          );
+}
 
 
 int main()
 {
      try
      {
+          initializeLogger( "using-spdlog" );
+
           spdlog::info("Welcome to spdlog!");
           spdlog::error("Some error message with arg: {}", 1);
 
